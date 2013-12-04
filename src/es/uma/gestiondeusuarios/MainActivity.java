0597152674e3e1104ad430dc7bc729264fc5616e
Gonzalo Fernández Jaime <jofera@gmail.com>
2013-12-04 16:42:26 +0100

    Sólo he creado la interfaz del login

---
+++ src/es/uma/gestiondeusuarios/MainActivity.java
@@ -0,0 +1,55 @@
+package es.uma.gestiondeusuarios;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Código para el botón de Login
+        final Button botonLogin = (Button) findViewById(R.id.botonLogin);
+        botonLogin.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				final EditText campoUsuario = (EditText) findViewById(R.id.campoUsuario);
+				final EditText campoPassword = (EditText) findViewById(R.id.campoPassword);
+				if((campoUsuario.getText().toString().isEmpty()) && 
+						(campoPassword.getText().toString().isEmpty())){
+					Toast.makeText(getApplicationContext(), R.string.ErrorLoginVacio, Toast.LENGTH_LONG).show();
+				}
+			}
+		});
+        // Código para el botón de Limpiar
+        final Button botonLimpiar = (Button) findViewById(R.id.botonLimpiar);
+        botonLimpiar.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				final EditText campoUsuario = (EditText) findViewById(R.id.campoUsuario);
+				final EditText campoPassword = (EditText) findViewById(R.id.campoPassword);
+				campoUsuario.setText("");
+				campoPassword.setText("");
+			}
+		});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
